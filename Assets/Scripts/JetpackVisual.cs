992728a1d1b0a7e6d724d821096fea5e76db8bb6
ToxToxx <107263231+ToxToxx@users.noreply.github.com>
2023-09-13 16:44:01 +0300

    separate logic of jetpack with animator

--- Assets/Scripts/JetpackVisual.cs
+++ Assets/Scripts/JetpackVisual.cs
@@ -2,35 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerAnimator : MonoBehaviour
+public class JetpackVisual : MonoBehaviour
 {
-    private Animator playerAnimator;
-
-    private const string IS_WALKING_ANIMATION = "IsWalking";
     [SerializeField] private GameObject onJetpackVisual;
     [SerializeField] private GameObject offJetpackVisual;
-
-    private void Start()
+    // Start is called before the first frame update
+    void Start()
     {
-        playerAnimator = GetComponent<Animator>();
+        
     }
 
-    private void Update()
+    // Update is called once per frame
+    void Update()
     {
-        StartWalking();
         StartFlyingJetpack();
-    }
-
-    private void StartWalking()
-    {
-        if (Player.Instance.GetIsWalking())
-        {
-            playerAnimator.SetBool(IS_WALKING_ANIMATION, true);
-        }
-        else
-        {
-            playerAnimator.SetBool(IS_WALKING_ANIMATION, false);
-        }
     }
 
     private void StartFlyingJetpack()
