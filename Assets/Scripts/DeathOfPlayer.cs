be5d0aacc19e9b6a28252ded0c03c2ab9149c155
ToxToxx <107263231+ToxToxx@users.noreply.github.com>
2023-09-14 20:56:16 +0300

    made death screen

--- Assets/Scripts/DeathOfPlayer.cs
+++ Assets/Scripts/DeathOfPlayer.cs
@@ -1,17 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class DeathOfPlayer : MonoBehaviour
 {
     [SerializeField] private Player player;
+    [SerializeField] private GameObject deathScreen;
 
     // Update is called once per frame
+    private void Start()
+    {
+        deathScreen.SetActive(false);
+    }
     void Update()
     {
         if (player.transform.position.y < -40)
         {
             Destroy(player.gameObject);
         }
+    }
+
+    private void OnDestroy()
+    {
+        deathScreen.SetActive(true);
     }
 }
